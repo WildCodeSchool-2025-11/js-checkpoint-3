bb6340313128e016618c9ed1867032c10357d82b
Victor dS <victordespirlet@gmail.com>
2026-03-26 11:47:22 +0100

    step 3, merci claude

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -12,6 +12,18 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const validate: RequestHandler = async (req, res, next) => {
   // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+    const tile = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+    if (tile) {
+      next();
+    } else {
+      res.status(422).json({ error: "Invalid coordinates" });
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
