ec6f52124696a162c5a2be109970de609955a91d
manuloup <galland.manuloup@gmail.com>
2026-03-26 11:47:40 +0100

    etape 3

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -4,19 +4,20 @@ import boatRepository from "./boatRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
   try {
-    // Fetch all boats from the database
     const boats = await boatRepository.readAll();
-
-    // Respond with the boats in JSON format
     res.json(boats);
   } catch (err) {
-    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    await boatRepository.update({ id: Number(req.params.id), ...req.body });
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -11,18 +11,18 @@ type Boat = {
 
 class BoatRepository {
   async readAll(where?: { name: string }) {
-    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await databaseClient.query<Rows>(
       "select * from boat order by coord_y, coord_x",
     );
-
-    // Return the array of tiles
     return rows as Boat[];
   }
 
   async update(boatToUpdate: Partial<Boat>) {
-    // your code here
-    return 0;
+    const [result] = await databaseClient.query<Result>(
+      "UPDATE boat SET coord_x = ?, coord_y = ? WHERE id = ?",
+      [boatToUpdate.coord_x, boatToUpdate.coord_y, boatToUpdate.id],
+    );
+    return (result as Result).affectedRows;
   }
 }
 
