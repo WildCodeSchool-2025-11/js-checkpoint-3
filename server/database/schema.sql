ed908f4ddb269e776d2d7d02889b98d74475a360
Marine_O <marine.opsommer@gmail.com>
2026-03-26 11:09:31 +0100

    step 1 done

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,3 +1,4 @@
+-- Active: 1771947141577@@127.0.0.1@3306@mysql
 CREATE TABLE boat (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(255) NOT NULL,
@@ -11,6 +12,7 @@ CREATE TABLE tile (
     type VARCHAR(255) NOT NULL,
     coord_x INT NOT NULL,
     coord_y INT NOT NULL,
+    has_treasure BOOLEAN NOT NULL DEFAULT false,
     PRIMARY KEY (id)
 );
 
