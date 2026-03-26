fa526b95f93ae84c545dd35d996aa0a2109890ba
Prescillia Rosart <prescillia.rosart@gmail.com>
2026-03-26 11:27:47 +0100

    step 3 PUT /api/boats/:id

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -16,7 +16,14 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const boatId = Number(req.params.id);
+    const { coord_x, coord_y } = req.body;
+    await boatRepository.update({ id: boatId, coord_x, coord_y });
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
