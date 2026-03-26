33e121dbd2c4d8a5300ef817cf8001a94642b74b
Kevan Gautier <kevangautier@gmail.com>
2026-03-26 11:51:09 +0100

    step 4 terminé après une bonne galère

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -11,7 +11,20 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const coordX = req.body.coord_x;
+    const coordY = req.body.coord_y;
+
+    const tiles = await tileRepository.readByCoordinates(coordX, coordY);
+
+    if (tiles.length > 0) {
+      next();
+    } else {
+      res.sendStatus(422);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
