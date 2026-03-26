7d8e4eca490e0159178cb64b4557b9874f57b957
Marine_O <marine.opsommer@gmail.com>
2026-03-26 11:42:02 +0100

    step 4 done

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -11,7 +11,19 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const tiles = await tileRepository.readByCoordinates(
+      req.body.coord_x,
+      req.body.coord_y,
+    );
+    if (tiles.length > 0) {
+      next();
+    } else {
+      res.sendStatus(422);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
--- server/src/router.ts
+++ server/src/router.ts
@@ -15,7 +15,7 @@ import tileActions from "./modules/tile/tileActions";
 
 router.post("/api/games", gameActions.add);
 router.get("/api/tiles", tileActions.browse);
-router.put("/api/boats/:id", boatActions.edit);
+router.put("/api/boats/:id", tileActions.validate, boatActions.edit);
 
 /* ************************************************************************* */
 
